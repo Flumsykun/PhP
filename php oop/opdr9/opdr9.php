b670dcb7c7f0ea9a9c9db54f49e19ac9f0be3923
Flumsykun <85942@glr.nl>
2023-01-27 14:35:33 +0100

    hij werkt bijna

--- php oop/opdr9/opdr9.php
+++ php oop/opdr9/opdr9.php
@@ -1,44 +1,82 @@
 <?php
-    require_once "auto.php";
-    require_once "EigenAuto.php";
-    require_once "LeaseAuto.php";
+require_once "auto.php";
 
-    echo "Auto's <br>";
-    $auto1 = new Auto("Audi", "A4", "blauw", true, "12-AB-34", 165465, 59, 20, 1.5);
-    $auto1->tanken(40);
-    echo "Benzine: " . $auto1->benzine . "<br>";
-    $auto1->rijden(15);
-    echo "Kilometers: " . $auto1->kilometers;
-    
-    echo "<br>";
-    echo "<br>";
-       
-    echo "Lease Auto's <br>";
-    $leaseAuto = new LeaseAuto('Audi','A4','blue',true,'12-AB-34',165465,59,20,1.5,'LeaseCompany Inc.', '2022-01-01','2025-01-01');
-    $leaseAuto->tanken(40);
-    echo "Benzine: " . $leaseAuto->benzine . "<br>";
-    $leaseAuto->rijden(15);
-    echo "Kilometers: " . $leaseAuto->kilometers. "<br>";
-    echo "Kenteken: " . $leaseAuto->kenteken. "<br>";
-    echo "Leasebedrijf: " . $leaseAuto->leasemaatschappij. "<br>";
-    echo "Lease start: " . $leaseAuto->start_contract. "<br>";
-    echo "Lease eind: " . $leaseAuto->einde_contract. "<br>";
-    
-    echo "<br>";
-    echo "<br>";
+session_start();
+if (!isset($_SESSION['cars'])) {
+    $_SESSION['cars'] = array();
+}
 
-    echo "Eigen Auto's <br>";
-    $eigenAuto = new EigenAuto('Audi','A4','blue',true,'12-AB-34',165465,59,20,1.5,'LeaseCompany Inc.', '2022-01-01','2025-01-01');
-    $eigenAuto->tanken(40);
-    echo "Benzine: " . $eigenAuto->benzine . "<br>";
-    $eigenAuto->rijden(15);
-    echo "Kilometers: " . $eigenAuto->kilometers. "<br>";
-    echo "Kenteken: " . $eigenAuto->kenteken. "<br>";
-    echo "Leasebedrijf: " . $leaseAuto->leasemaatschappij. "<br>";
-    echo "Lease start: " . $leaseAuto->start_contract. "<br>";
-    echo "Lease eind: " . $leaseAuto->einde_contract. "<br>";
+if (isset($_POST['submit'])) {
+    $car = new Auto($_POST['merk'], $_POST['type'], $_POST['kleur'], $_POST['trekhaak'], $_POST['kenteken'], $_POST['kilometers'], $_POST['benzine'], $_POST['tankinhoud'], $_POST['verbruik']);
+    $_SESSION['cars'][] = $car;
+}
 
-    echo "<br>";
-    echo "<br>";
+echo "<form method='post'>";
+echo "Merk: <input type='text' name='merk'><br>";
+echo "Type: <input type='text' name='type'><br>";
+echo "Kleur: <input type='text' name='kleur'><br>";
+echo "Heeft trekhaak: <input type='checkbox' name='trekhaak'><br>";
+echo "Kenteken: <input type='text' name='kenteken'><br>";
+echo "Kilometers: <input type='number' name='kilometers'><br>";
+echo "Benzine: <input type='number' name='benzine'><br>";
+echo "Tankinhoud: <input type='number' name='tankinhoud'><br>";
+echo "Verbruik: <input type='number' name='verbruik'><br>";
+echo "<input type='submit' name='submit' value='Submit'>";
+echo "</form>";
+
+echo "<br> <br>";
+
+foreach ($_SESSION['cars'] as $car) {
+    echo "Merk: " . $car->merk . "<br>";
+    echo "Type: " . $car->type . "<br>";
+    echo "Kleur: " . $car->kleur . "<br>";
+    echo "Heeft trekhaak: " . $car->heeftTrekhaak . "<br>";
+    echo "Kenteken: " . $car->kenteken . "<br>";
+    echo "Kilometers: " . $car->kilometers . "<br>";
+    echo "Benzine: " . $car->benzine . "<br>";
+    echo "Tankinhoud: " . $car->tankinhoud . "<br>";
+    echo "Verbruik: " . $car->verbruik . "<br>";
+    echo "<br> <br>";
+}
+
+
+// session_start();
+
+// if (!isset($_SESSION['auto1'])) {
+//     echo "Auto's <br>";
+//     $auto1 = new Auto("Audi", "A4", "blauw", true, "12-AB-34", 165465, 59, 20, 1.5);
+//     $_SESSION['auto1'] = $auto1;
+// } else {
+//     $auto1 = $_SESSION['auto1'];
+// }
+
+// $auto1->tanken(40);
+// $auto1->rijden(15);
+// echo "Merk: " . $auto1->merk . "<br>";
+// echo "Type: " . $auto1->type . "<br>";
+// echo "Kleur: " . $auto1->kleur . "<br>";
+// echo "Heeft trekhaak: " . $auto1->heeftTrekhaak . "<br>";
+// echo "Kenteken: " . $auto1->kenteken . "<br>";
+// echo "Kilometers: " . $auto1->kilometers . "<br>";
+// echo "Benzine: " . $auto1->benzine . "<br>";
+// echo "Tankinhoud: " . $auto1->tankinhoud . "<br>";
+// echo "Verbruik: " . $auto1->verbruik . "<br>";
+
+// echo "<br>";
+// echo "<br>";
+   
+// echo "Lease Auto's <br>";
+// $leaseAuto = new LeaseAuto('Audi','A4','blue',true,'12-AB-34',165465,59,20,1.5,'LeaseCompany Inc.', '2022-01-01','2025-01-01');
+// $leaseAuto->tanken(40);
+// echo "Benzine: " . $leaseAuto->benzine . "<br>";
+// $leaseAuto->rijden(15);
+// echo "Kilometers: " . $leaseAuto->kilometers. "<br>";
+// echo "Kenteken: " . $leaseAuto->kenteken. "<br>";
+// echo "Leasebedrijf: " . $leaseAuto->leasemaatschappij. "<br>";
+// echo "Lease start: " . $leaseAuto->start_contract. "<br>";
+// echo "Lease eind: " . $leaseAuto->einde_contract. "<br>";
+
+// echo "<br>";
+// echo "<br>";
 
 ?>
